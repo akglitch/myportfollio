1517a455ee2b1177692aeedf0b6e1d8b2c101560
akglitch <91568087+akglitch@users.noreply.github.com>
2022-06-02 00:48:47 -0700

    Update myprojects.js

--- src/components/assets/myprojects.js
+++ src/components/assets/myprojects.js
@@ -11,7 +11,7 @@ export const projects = [
   {
     'id': 2,
     "name": 'Quote Generator',
-    "desc": 'A web app which displays quote by random people around the world',
+    "desc": 'A web app which displays quote by random people around the world.',
     'site': 'https://kteequotegen.netlify.app',
     'img':require('../utils/imgs/quote.png'),
     'github': "https://github.com/akglitch/Quote-Generator",
@@ -21,7 +21,7 @@ export const projects = [
   {
     'id': 3,
     "name": 'BlueHut',
-    "desc": 'Bluehut is a site which was built for an engeneering firm located in Accra  ',
+    "desc": 'Bluehut is a site which was built for an engeneering firm located in Accra.',
     'site': 'http://bluehutengineering.netlify.app',
     'img':require('../utils/imgs/bluehut.png'),
     'github': "http://github.com/dglitxh/Bluehut",
